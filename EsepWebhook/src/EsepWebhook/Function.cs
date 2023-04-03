7f384e1e3a6f6eeda16fb6098f0c437ebe52fac0
danielcamejo <dcamejo1@ufl.edu>
2023-04-03 15:47:47 +0200

    Fixed Json Parser

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -19,7 +19,7 @@ public class Function
     public string FunctionHandler(string input, ILambdaContext context)
     {
 
-        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+        dynamic json = Newtonsoft.JSON.JsonConvert.DeserializeObject<dynamic>(input.ToString());
 
         string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
 
